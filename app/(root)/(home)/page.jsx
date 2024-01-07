f0961d67408949dd55ffd2d441821f993c88f323
Taras Savchenko <hiddenroadss@gmail.com>
2024-01-07 13:24:36 +0200

    add basic clerk auth

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
 
 export default Home;
